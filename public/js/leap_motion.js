828a5ffca98eb2f2cd0f118b026fce4b779c4db6
Tulun <jaykiraly@gmail.com>
2015-10-15 15:37:29 -0700

    working.

--- public/js/leap_motion.js
+++ public/js/leap_motion.js
@@ -21,74 +21,75 @@ function move(frame) {
 
     // if(previousFrame && previousFrame.valid)
     //Stopping Leap Motion
+    var previousFrame ||= null;
     var pov = panorama.getPov();
     var panoNum = null;
-    if(frame.valid && frame.hands.length > 0) {
-      for (var i = 0; i < frame.hands.length; i++) {
-        var hand = frame.hands[i];
-        if (hand.grabStrength >0.85) {
-          break
-        } else {
-          if (previousFrame) {
-            var axis = hand.rotationAxis(previousFrame);
-            // console.log(axis[2]);
-            if (axis[2] > 0.95) {
-              panorama.setPov({
-              heading: pov.heading + 5,
-              pitch:0});
-            };
-            if (axis[2] < -0.95) {
-              panorama.setPov({
-                heading: pov.heading - 5,
-              pitch:0})
-            };
-            if (hand.palmPosition[2]) {
-              var palm = hand.palmPosition[2];
-              if (palm < -30) {
-                links = panorama.getLinks();
-                  if (links !== undefined) {
-                    if (links.length > 1) {
-                      pano0 = Math.abs(links[0]['heading'] - pov.heading);
-                      pano1 = Math.abs(links[1]['heading'] - pov.heading);
-                      if (pano0 < pano1) {
-                         panoNum = 0;
-                      } else {
-                        panoNum = 1;
-                      }
-                    panorama.setPano(links[panoNum]['pano']);
-                    } else {
-                      panorama.setpano(links[0]['pano']);
-                    }
-                  console.log('forward');
-                  console.log(links);
-              }
-            };
-            if (palm > 30) {
-             links = panorama.getLinks();
-              if (links !== undefined) {
-                  if (links.length > 1) {
-                      pano0 = Math.abs(links[0]['heading'] - pov.heading);
-                      pano1 = Math.abs(links[1]['heading'] - pov.heading);
-                      if (pano0 < pano1 ) {
+      if(frame.valid && frame.hands.length > 0) {
+        for (var i = 0; i < frame.hands.length; i++) {
+          var hand = frame.hands[i];
+          if (hand.grabStrength >0.85) {
+            break
+          } else {
+            if (previousFrame) {
+              var axis = hand.rotationAxis(previousFrame);
+              // console.log(axis[2]);
+              if (axis[2] > 0.95) {
+                panorama.setPov({
+                heading: pov.heading + 5,
+                pitch:0});
+              };
+              if (axis[2] < -0.95) {
+                panorama.setPov({
+                  heading: pov.heading - 5,
+                pitch:0})
+              };
+              if (hand.palmPosition[2]) {
+                var palm = hand.palmPosition[2];
+                if (palm < -30) {
+                  links = panorama.getLinks();
+                    if (links !== undefined) {
+                      if (links.length > 1) {
+                        pano0 = Math.abs(links[0]['heading'] - pov.heading);
+                        pano1 = Math.abs(links[1]['heading'] - pov.heading);
+                        if (pano0 < pano1) {
+                           panoNum = 0;
+                        } else {
                           panoNum = 1;
-                      }
-                      else {
-                          panoNum = 0;
-                      }
+                        }
                       panorama.setPano(links[panoNum]['pano']);
-                  } else {
-                      panorama.setpano(links[0]['pano']);
-                  }
-                  console.log('back');
-                  console.log(links);
-              }
+                      } else {
+                        panorama.setpano(links[0]['pano']);
+                      }
+                    console.log('forward');
+                    console.log(links);
+                }
+              };
+              if (palm > 30) {
+               links = panorama.getLinks();
+                if (links !== undefined) {
+                    if (links.length > 1) {
+                        pano0 = Math.abs(links[0]['heading'] - pov.heading);
+                        pano1 = Math.abs(links[1]['heading'] - pov.heading);
+                        if (pano0 < pano1 ) {
+                            panoNum = 1;
+                        }
+                        else {
+                            panoNum = 0;
+                        }
+                        panorama.setPano(links[panoNum]['pano']);
+                    } else {
+                        panorama.setpano(links[0]['pano']);
+                    }
+                    console.log('back');
+                    console.log(links);
+                }
 
+              }
             }
-          }
-          }
+            }
+          };
         };
       };
-    };
 
 
   previousFrame = frame;
